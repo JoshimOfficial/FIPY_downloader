9e3cd3edb39a4c62d148ae5ef33d68bcb26b06e7
Joshim Uddin <idkhs8211@gmail.com>
2023-02-08 12:57:22 +0600

    fixed pinterest information table

--- assets/files/scraper/pinterest/get_vid.php
+++ assets/files/scraper/pinterest/get_vid.php
@@ -72,7 +72,7 @@ if(isset($_COOKIE["_PINTEREST_VID_LINK"])) {
 
 
                <div class="relative overflow-x-auto shadow-md sm:rounded-lg mt-5 md:mt-0">
-    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+    <table class="w-full md:max-w-xl text-sm text-left text-gray-500 dark:text-gray-400">
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
                 <th scope="col" class="px-6 py-3">
